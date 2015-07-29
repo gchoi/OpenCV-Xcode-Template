449f120f77e2f6856b7a307a9316e1383e794c83
Geol Choi <cinema4dr12@gmail.com>
2015-07-29 21:36:02 +0900

    2015-07-29 21:35

---
+++ opencv-xcode-template/HarrisCornerDetector.cpp
@@ -0,0 +1,76 @@
+//
+//  HarrisCornerDetector.cpp
+//  opencv-xcode-template
+//
+//  Created by gchoi on 7/25/15.
+//  Copyright (c) 2015 gchoi. All rights reserved.
+//
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+cv::Mat src, src_gray;
+int thresh = 200;
+int max_thresh = 255;
+char* source_window = "Source image";
+char* corners_window = "Corners detected";
+void cornerHarris_demo( int, void* );
+
+
+/* @ function main */
+int main( int argc, char** argv )
+{
+    
+    src = cv::imread( "../../../../../res/building.png", 1 );
+    
+    cv::cvtColor( src, src_gray, COLOR_BGR2GRAY );
+    
+    cv::namedWindow( source_window, WINDOW_AUTOSIZE );
+    
+    cv::createTrackbar( "Threshold: ", source_window, &thresh, max_thresh, cornerHarris_demo );
+    
+    cv::imshow( source_window, src );
+    
+    cornerHarris_demo( 0, 0 );
+    
+    cv::waitKey(0);
+    
+    return(0);
+    
+}
+
+
+/* @function cornerHarris_demo */
+void cornerHarris_demo( int, void* )
+{
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros( src.size(), CV_32FC1 );
+    
+    int blockSize = 2;
+    int apertureSize = 3;
+    double k = 0.04;
+    
+    cv::cornerHarris( src_gray, dst, blockSize, apertureSize, k, BORDER_DEFAULT );
+    
+    cv::normalize( dst, dst_norm, 0, 255, NORM_MINMAX, CV_32FC1, Mat() );
+    
+    cv::convertScaleAbs( dst_norm, dst_norm_scaled );
+    
+    for( int j = 0; j < dst_norm.rows ; j++ ) {
+        for( int i = 0; i < dst_norm.cols; i++ ) {
+            if( (int) dst_norm.at<float>(j,i) > thresh ) {
+                cv::circle( dst_norm_scaled, Point( i, j ), 5,  Scalar(0), 2, 8, 0 );
+            }
+        }
+    }
+    
+    cv::namedWindow( corners_window, WINDOW_AUTOSIZE );
+    
+    cv::imshow( corners_window, dst_norm_scaled );
+}
